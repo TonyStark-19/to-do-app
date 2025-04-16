4c4184255c22ab4f165cf35ada7b7c5350ee0573
Aditya chandel <adityachandel371@gmail.com>
2025-04-16 21:24:09 +0530

    Added layout and css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+// css file
 import './App.css';
 
-function App() {
+// icons
+import { FaRegCircle, FaCheckCircle, FaTrash, FaPlus } from 'react-icons/fa';
+import { SlOptions } from "react-icons/sl";
+
+// main page
+
+export default function TodoApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Navbar />
+      <Task />
     </div>
   );
 }
 
-export default App;
+// navbar
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='date'>
+        <h2>My Day</h2>
+        <p>{GetDate()}</p>
+      </div>
+      <div className='options'>
+        <div className='icon-wrap'>
+          <SlOptions className='options-icon' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// get date function
+
+function GetDate() {
+  const today = new Date();
+
+  const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August",
+    "September", "October", "November", "December"];
+
+  const weekday = weekDays[today.getDay()];
+  const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
+  const month = allMonths[today.getMonth()];
+
+  return `${weekday}, ${day} ${month}`;
+}
+
+// tasks
+
+function Task() {
+  return (
+    <div className='tasks'>
+      <div className='task-container'>
+        <TaskElements />
+      </div>
+      <div className='task-input-box'>
+        <FaPlus className="add-icon" />
+        <input type='text' placeholder='Add Task'></input>
+      </div>
+    </div>
+  )
+}
+
+// task elements
+
+function TaskElements() {
+  return (
+    <div className='task-item'>
+      <div className='task-text-wrap'>
+        <FaRegCircle className='task-icon' />
+        <span className='task-text'>Finish coding</span>
+      </div>
+      <FaTrash className='delete-icon' />
+    </div>
+  )
+}
